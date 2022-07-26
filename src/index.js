3550d94ee9152db1aa02b00e82b82f1423c671eb
chin-cyber <chiragsaraswat@jklu.edu.in>
2022-07-26 12:44:31 +0530

    created signup and login page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'tachyons';
+import App from './containers/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import 'tachyons';
-import App from './containers/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
